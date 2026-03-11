50fc2e387e245946f7d249d8d8d161bd21a658f7
Godotheawful <geraldo.neto267@al.unieduk.com.br>
2026-03-10 21:45:04 -0300

    Add files via upload

---
+++ cofresenha.py
@@ -0,0 +1,20 @@
+#geraldo
+#2026
+
+print(" ################################################################# ")
+print("                     COFRE BANCÁRIO VALIOSO                        ")
+print(" ################################################################# ")
+
+
+
+
+a = input("Digite a senha secreta.")
+
+if (a == "batata123"):
+	print ("Parabéns! você está rico.")
+else:
+	print ("Senha incorreta, tente novamente.")
+
+
+input()
+	
